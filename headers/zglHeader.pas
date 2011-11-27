8cab109253352c4836f872bcd8e2bf2a2651fb12
dr.andru <andru-kun@users.noreply.github.com>
2011-11-27 11:05:09 +0000

    updated zendx branch

--- headers/zglHeader.pas
+++ headers/zglHeader.pas
@@ -3,7 +3,7 @@
 {--------------------------------}
 {                                }
 { version:  0.3                  }
-{ date:     2011.11.12           }
+{ date:     2011.11.27           }
 { license:  GNU LGPL version 3   }
 { homepage: http://zengl.org     }
 {                                }
