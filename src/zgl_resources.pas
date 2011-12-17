ce28cbc60ca8b4b4440dd5315dc2afd5ca4e61cb
dr.andru <andru-kun@users.noreply.github.com>
2011-12-17 16:54:54 +0000

    fixed compatibility with Delphi XE 2

--- src/zgl_resources.pas
+++ src/zgl_resources.pas
@@ -148,7 +148,7 @@ var
   resCompleted       : Integer;
   resThread          : array[ 0..255 ] of {$IFNDEF ANDROID} LongWord {$ELSE} ppthread_t {$ENDIF};
   {$IFNDEF FPC}
-  resThreadID        : array[ 0..255 ] of THandle;
+  resThreadID        : array[ 0..255 ] of LongWord;
   {$ENDIF}
   resQueueStackID    : array of Byte;
   resQueueID         : array[ 0..255 ] of Byte;
