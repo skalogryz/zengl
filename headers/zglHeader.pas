f6afefb75a836aed973a6a50199575773b4f3333
dr.andru <andru-kun@users.noreply.github.com>
2012-04-30 11:19:50 +0000

    [Windows]fixed text input

--- headers/zglHeader.pas
+++ headers/zglHeader.pas
@@ -3,7 +3,7 @@
 {--------------------------------}
 {                                }
 { version:  0.3 beta 1           }
-{ date:     2012.04.27           }
+{ date:     2012.04.30           }
 { license:  GNU LGPL version 3   }
 { homepage: http://zengl.org     }
 {                                }
