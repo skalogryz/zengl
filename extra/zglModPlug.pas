f1b3921198117ed9c1d234a352a2db9d9b23d86f
dr.andru <andru-kun@users.noreply.github.com>
2012-05-13 13:59:50 +0000

    [Android]added compiled libmodplug; fixed zglModPlug.pas

--- extra/zglModPlug.pas
+++ extra/zglModPlug.pas
@@ -1,14 +1,10 @@
 unit zglModPlug;
 
-// RU: Если проект не собирается с ZenGL статически, то стоит закоментировать этот define
-// EN: If project doesn't compile statically with ZenGL then comment define below
-{$DEFINE STATIC}
+{$I zglCustomConfig.cfg}
 
 interface
 uses
-  {$IFNDEF STATIC}
-  zglHeader,
-  {$ELSE}
+  {$IFDEF USE_ZENGL_STATIC}
   zgl_types,
   zgl_application,
   zgl_main,
@@ -17,6 +13,8 @@ uses
   zgl_file,
   zgl_memory,
   zgl_utils
+  {$ELSE}
+  zglHeader
   {$ENDIF}
   ;
 
@@ -31,6 +29,11 @@ const
   libmodplug = 'libmodplug.dylib';
 {$ENDIF}
   MAX_FORMATS = 22;
+
+procedure InitModPlug;
+procedure FreeModPlug;
+
+implementation
 
 const
   MODPLUG_ENABLE_OVERSAMPLING     = 1 shl 0;
@@ -49,13 +52,6 @@ type
   ModPlugFile = record
   end;
 
-procedure mp_Init;
-procedure mp_Free;
-function  mp_DecoderOpen( var Stream : zglTSoundStream; const FileName : UTF8String ) : Boolean;
-function  mp_DecoderRead( var Stream : zglTSoundStream; Buffer : Pointer; Bytes : DWORD; var _End : Boolean ) : DWORD;
-procedure mp_DecoderLoop( var Stream : zglTSoundStream );
-procedure mp_DecoderClose( var Stream : zglTSoundStream );
-
 var
   Decoders : array[ 0..MAX_FORMATS - 1 ] of zglTSoundDecoder;
   FORMATS  : array[ 0..MAX_FORMATS - 1 ] of UTF8String = ( 'MOD', 'IT',  'S3M', 'XM',  'IT',  '669', 'AMF', 'AMS', 'DBM', 'DMF', 'DSM', 'FAR',
@@ -71,29 +67,85 @@ var
   ModPlug_Seek      : procedure(_file: PModPlugFile; millisecond: longint); cdecl;
   ModPlug_GetLength : function(_file: PModPlugFile): longint; cdecl;
 
-implementation
-
-procedure mp_Init;
+function mp_DecoderOpen( var Stream : zglTSoundStream; const FileName : UTF8String ) : Boolean;
   var
-    i : Integer;
+    mem : zglTMemory;
 begin
-  for i := 0 to MAX_FORMATS - 1 do
-    begin
-      Decoders[ i ].Ext   := FORMATS[ i ];
-      Decoders[ i ].Open  := mp_DecoderOpen;
-      Decoders[ i ].Read  := mp_DecoderRead;
-      Decoders[ i ].Loop  := mp_DecoderLoop;
-      Decoders[ i ].Close := mp_DecoderClose;
-      zgl_Reg( SND_FORMAT_EXTENSION, @FORMATS[ i ] );
-      zgl_Reg( SND_FORMAT_FILE_LOADER, nil );
-      zgl_Reg( SND_FORMAT_MEM_LOADER,  nil );
-      zgl_Reg( SND_FORMAT_DECODER, @Decoders[ i ] );
-    end;
+  if not mpLoad Then InitModPlug();
+  if not mpInit Then exit;
 
+  mem_LoadFromFile( mem, FileName );
+  PModPlugFile( Stream._data ) := ModPlug_Load( mem.Memory, mem.Size );
+  mem_Free( mem );
+
+  if Assigned( Stream._data ) Then
+    begin
+      Result := TRUE;
+
+      Stream.Bits       := 16;
+      Stream.Frequency  := 44100;
+      Stream.Channels   := 2;
+      Stream.Duration   := ModPlug_GetLength( PModPlugFile( Stream._data ) );
+      Stream.BufferSize := 64 * 1024;
+      zgl_GetMem( Pointer( Stream.Buffer ), Stream.BufferSize );
+    end else
+      Result := FALSE;
+end;
+
+function mp_DecoderOpenMem( var Stream : zglTSoundStream; const Memory : zglTMemory ) : Boolean;
+begin
+  if not mpLoad Then InitModPlug;
+  if not mpInit Then exit;
+
+  PModPlugFile( Stream._data ) := ModPlug_Load( Memory.Memory, Memory.Size );
+
+  if Assigned( Stream._data ) Then
+    begin
+      Result := TRUE;
+
+      Stream.Bits       := 16;
+      Stream.Frequency  := 44100;
+      Stream.Channels   := 2;
+      Stream.Duration   := ModPlug_GetLength( PModPlugFile( Stream._data ) );
+      Stream.BufferSize := 64 * 1024;
+      zgl_GetMem( Pointer( Stream.Buffer ), Stream.BufferSize );
+    end else
+      Result := FALSE;
+end;
+
+function mp_DecoderRead( var Stream : zglTSoundStream; Buffer : PByteArray; Bytes : LongWord; out _End : Boolean ) : LongWord;
+begin
+  if not mpInit Then exit;
+
+  Result := ModPlug_Read( PModPlugFile( Stream._data ), @Buffer[ 0 ], Bytes );
+  _End := Result = 0;
+end;
+
+procedure mp_DecoderLoop( var Stream : zglTSoundStream );
+begin
+  if not mpInit Then exit;
+
+  ModPlug_Seek( PModPlugFile( Stream._data ), 0 );
+end;
+
+procedure mp_DecoderClose( var Stream : zglTSoundStream );
+begin
+  if not mpInit Then exit;
+
+  ModPlug_Unload( PModPlugFile( Stream._data ) );
+  Stream._data := nil;
+end;
+
+procedure InitModPlug;
+begin
   {$IFDEF LINUX}
+  {$IFNDEF ANDROID}
   mpLibrary := dlopen( PAnsiChar( './' + libmodplug + '.1' ), $001 );
   if mpLibrary = LIB_ERROR Then mpLibrary := dlopen( PAnsiChar( libmodplug + '.1' ), $001 );
   if mpLibrary = LIB_ERROR Then mpLibrary := dlopen( PAnsiChar( libmodplug + '.0' ), $001 );
+  {$ELSE}
+  mpLibrary := dlopen( libmodplug, $001 );
+  {$ENDIF}
   {$ENDIF}
   {$IFDEF WINDOWS}
   mpLibrary := dlopen( libmodplug );
@@ -121,64 +173,32 @@ begin
   mpLoad := TRUE;
 end;
 
-procedure mp_Free;
+procedure FreeModPlug;
 begin
   mpInit := FALSE;
   dlclose( mpLibrary );
 end;
 
-function mp_DecoderOpen( var Stream : zglTSoundStream; const FileName : String ) : Boolean;
-  var
-    mem : zglTMemory;
-begin
-  if not mpLoad Then mp_Init;
-  if not mpInit Then exit;
-
-  mem_LoadFromFile( mem, FileName );
-  PModPlugFile( Stream._data ) := ModPlug_Load( mem.Memory, mem.Size );
-  mem_Free( mem );
-
-  if Assigned( Stream._data ) Then
-    begin
-      Result := TRUE;
-
-      Stream.Bits       := 16;
-      Stream.Frequency  := 44100;
-      Stream.Channels   := 2;
-      Stream.Length     := ModPlug_GetLength( PModPlugFile( Stream._data ) );
-      Stream.BufferSize := 64 * 1024;
-      zgl_GetMem( Pointer( Stream.Buffer ), Stream.BufferSize );
-    end else
-      Result := FALSE;
-end;
-
-function mp_DecoderRead( var Stream : zglTSoundStream; Buffer : Pointer; Bytes : DWORD; var _End : Boolean ) : DWORD;
-begin
-  if not mpInit Then exit;
-
-  Result := ModPlug_Read( PModPlugFile( Stream._data ), Buffer, Bytes );
-  _End := Result = 0;
-end;
-
-procedure mp_DecoderLoop( var Stream : zglTSoundStream );
-begin
-  if not mpInit Then exit;
-
-  ModPlug_Seek( PModPlugFile( Stream._data ), 0 );
-end;
-
-procedure mp_DecoderClose( var Stream : zglTSoundStream );
-begin
-  if not mpInit Then exit;
-
-  ModPlug_Unload( PModPlugFile( Stream._data ) );
-  Stream._data := nil;
-end;
-
+var
+  i : Integer;
 initialization
+for i := 0 to MAX_FORMATS - 1 do
+  begin
+    Decoders[ i ].Ext     := FORMATS[ i ];
+    Decoders[ i ].Open    := @mp_DecoderOpen;
+    Decoders[ i ].OpenMem := @mp_DecoderOpenMem;
+    Decoders[ i ].Read    := @mp_DecoderRead;
+    Decoders[ i ].Seek    := nil;
+    Decoders[ i ].Loop    := @mp_DecoderLoop;
+    Decoders[ i ].Close   := @mp_DecoderClose;
+    zgl_Reg( SND_FORMAT_EXTENSION, @FORMATS[ i, 1 ] );
+    zgl_Reg( SND_FORMAT_FILE_LOADER, nil );
+    zgl_Reg( SND_FORMAT_MEM_LOADER,  nil );
+    zgl_Reg( SND_FORMAT_DECODER, @Decoders[ i ] );
+  end;
 
 finalization
   if mpInit Then
-    mp_Free();
+    FreeModPlug();
 
 end.
