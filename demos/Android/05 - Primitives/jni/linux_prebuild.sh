349888f4475187dd28d249527fa8ee4e5af094a3
dr.andru <andru-kun@users.noreply.github.com>
2012-07-05 16:34:40 +0000

    [Android]updated demos to use armv6 libraries

--- demos/Android/05 - Primitives/jni/linux_prebuild.sh
+++ demos/Android/05 - Primitives/jni/linux_prebuild.sh
@@ -1,4 +1,4 @@
 # copy libraries
-cp ../../../../bin/Android/armv5/libchipmunk.so ../libs/armeabi/
-cp ../../../../bin/Android/armv5/libopenal.so ../libs/armeabi/
-cp ../../../../bin/Android/armv5/libzenjpeg.so ../libs/armeabi/
+cp ../../../../bin/Android/armv6/libchipmunk.so ../libs/armeabi/
+cp ../../../../bin/Android/armv6/libopenal.so ../libs/armeabi/
+cp ../../../../bin/Android/armv6/libzenjpeg.so ../libs/armeabi/
--- demos/Android/12 - Render into Texture/jni/linux_prebuild.sh
+++ demos/Android/12 - Render into Texture/jni/linux_prebuild.sh
@@ -1,7 +1,7 @@
 # copy libraries
-cp ../../../../bin/Android/armv5/libchipmunk.so ../libs/armeabi/
-cp ../../../../bin/Android/armv5/libopenal.so ../libs/armeabi/
-cp ../../../../bin/Android/armv5/libzenjpeg.so ../libs/armeabi/
+cp ../../../../bin/Android/armv6/libchipmunk.so ../libs/armeabi/
+cp ../../../../bin/Android/armv6/libopenal.so ../libs/armeabi/
+cp ../../../../bin/Android/armv6/libzenjpeg.so ../libs/armeabi/
 # copy resources
 cp ../../../../bin/data/tux_stand.png ../assets
 cp ../../../../bin/data/font* ../assets
