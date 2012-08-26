1b3016777dbf660870ac91a879bb99745480eacd
dr.andru <andru-kun@users.noreply.github.com>
2012-08-26 19:15:20 +0000

    [OpenGL]fixed problem with Ctrl+Alt+Delete and message "Cannot set fullscreen mode"\ fixed crash when cannot load some resources using res_* functions

--- headers/zglHeader.pas
+++ headers/zglHeader.pas
@@ -64,11 +64,11 @@ type
   {$ENDIF}
 
   PByteArray     = ^TByteArray;
-  TByteArray     = array[ 0..65535 ] of Byte;
+  TByteArray     = array[ 0..High(LongWord) shr 1 - 1 ] of Byte;
   PWordArray     = ^TWordArray;
-  TWordArray     = array[ 0..32767 ] of Word;
+  TWordArray     = array[ 0..High(LongWord) shr 2 - 1 ] of Word;
   PLongWordArray = ^TLongWordArray;
-  TLongWordArray = array[ 0..16383 ] of LongWord;
+  TLongWordArray = array[ 0..High(LongWord) shr 3 - 1 ] of LongWord;
 
 type
   zglTStringList = record
