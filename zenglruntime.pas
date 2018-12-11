dcae37255939d24f7841311ddbc87c11f8b5f9b4
Dmitry Boyarintsev <skalogryz.lists@gmail.com>
2018-12-11 11:28:47 -0500

    updating zengl package to hookup optional units. Todo: those should go into separate packages

--- zenglruntime.pas
+++ zenglruntime.pas
@@ -7,6 +7,10 @@ unit zenglruntime;
 {$warn 5023 off : no warning about unused units}
 interface
 
+uses
+  zgl_mouse_to_touch, zgl_sound_ogg, zgl_text, zgl_textures_jpg, 
+  zgl_textures_png, zgl_textures_tga;
+
 implementation
 
 end.
